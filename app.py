1c2bb4bb08440a9b7e8885c0f7a715e349e79077
Rupert <rupertjhewson@gmail.com>
2020-04-15 10:42:03 +0000

    tasks html

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello ():
 
 if __name__ == '__main__':
    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('5000')),
+            port=int(os.environ.get('PORT')),
             debug=True)
 
             
