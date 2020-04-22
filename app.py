e20d15a7a561742cde8c857a8fe7553d25b336a3
Rupert <rupertjhewson@gmail.com>
2020-04-22 10:29:54 +0000

    corns

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def get_categories():
         return render_template('categories.html', 
                                categories=mongo.db.categories.find())
         
-
-@app.route('/delete_category/<category_id')
+@app.route('/delete_category/<category_id>')
 def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
